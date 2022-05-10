7b7e899ebb5d7d968b7993958ef104c276477ff4
Florian Bischoff <mail@florian-bischoff.de>
2022-05-10 11:18:07 +0200

    fest: add dec union example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as COD from "io-ts/lib/Codec.js"
+import * as DEC from "io-ts/lib/Decoder.js"
 
 const a = COD.string;
 const b = a.decode(1);  // b resolves to Kind2<M, E, A>
-
+const c = DEC.union(DEC.string, DEC.number); // resolves to DEC.Decoder<unknown, unknown>
