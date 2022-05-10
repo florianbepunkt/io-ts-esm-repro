3cd03328eaac27325e8a3fe25d8f230ac14eb7c0
Florian Bischoff <mail@florian-bischoff.de>
2022-05-10 10:10:58 +0200

    feat: add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Set TS version to 4.7 and module config to CommonJS.
+
+See index.ts: Variable b is inferred correctly as Either.
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
+
 import * as COD from "io-ts/Codec"
-import * as E from "fp-ts/Either"
-import * as DEC from "io-ts/Decoder"
-import { identity } from "fp-ts/lib/function";
 
 const a = COD.string;
 const b = a.decode(1);  // b resolves to correct Either
 
-const c: COD.Codec<unknown, string, string> = {
-    decode: (i) => typeof i === "string" ? DEC.success(i) : DEC.failure(i, "Expected string"),
-    encode: identity
-}
-
-const d = c.decode("")
