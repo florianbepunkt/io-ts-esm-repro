9a27f56b8887363b3898bf011a11222db2c70312
Florian Bischoff <mail@florian-bischoff.de>
2022-05-10 10:05:41 +0200

    feat: cjs 4.7 example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-import * as COD from "io-ts/lib/Codec.js"
+import * as COD from "io-ts/Codec"
+import * as E from "fp-ts/Either"
+import * as DEC from "io-ts/Decoder"
+import { identity } from "fp-ts/lib/function";
 
 const a = COD.string;
 const b = a.decode(1);  // b resolves to Kind2<M, E, A>
 
+const c: COD.Codec<unknown, string, string> = {
+    decode: (i) => typeof i === "string" ? DEC.success(i) : DEC.failure(i, "Expected string"),
+    encode: identity
+}
+
+const d = c.decode("")
