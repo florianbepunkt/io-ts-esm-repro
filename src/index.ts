f7ebf40c4f5dd36848e4bf42ba86080baae6f803
Florian Bischoff <mail@florian-bischoff.de>
2022-05-10 11:17:30 +0200

    feat: add union example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 import * as COD from "io-ts/Codec"
+import * as DEC from "io-ts/lib/Decoder.js"
 
 const a = COD.string;
 const b = a.decode(1);  // b resolves to correct Either
-
+const c = DEC.union(DEC.string, DEC.number); // resolves correctly to DEC.Decoder<unknown, string | number>
